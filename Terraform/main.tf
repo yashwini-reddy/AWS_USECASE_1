340062851efac22fade4e4d822bca9b42f7b8fb3
yashwini-reddy <151424956+yashwini-reddy@users.noreply.github.com>
2025-06-22 14:53:49 +0530

    bucket name  update

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "static_website_bucket" {
-  bucket = "MyStaticWebsiteBucket1" # Unique bucket name
+  bucket = "my-bucket-static-website-hosting-usecase" # Unique bucket name
 
   website {
     index_document = "index.html"
